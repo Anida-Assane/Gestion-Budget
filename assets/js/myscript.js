6e5961370168260c271b8b8f0d88e9726b179811
Anida-Assane <anidaassaneyacouba18@gmail.com>
2022-09-20 03:31:47 +0200

    ajout de la deuxieme partie des fonctionnalités

--- assets/js/myscript.js
+++ assets/js/myscript.js
@@ -5,6 +5,8 @@ let number = document.getElementById("number");
 let span_text = document.getElementById("span-text");
 let span_number = document.getElementById("span-number");
 let inside_income = document.getElementById("inside-income");
+let inside_expenses = document.getElementById("inside-expenses");
+let check_icon = document.getElementById("check-icon");
 text.onblur = function (event) {
   if (event.target.value == "") {
     span_text.removeAttribute("hidden");
@@ -32,7 +34,7 @@ number.oninput = function (event) {
 
 number.onkeyup = function (touche) {
   if (touche.key == "Enter") {
-    if (text.value != "" && number.value != "") {
+    if (text.value != "" && number.value != "" && input_value.value == "+") {
       let render_income = document.createElement("div");
       render_income.innerHTML = `
         <div
@@ -42,15 +44,70 @@ number.onkeyup = function (touche) {
                     width: 100%;
                     display: flex;
                     justify-content: space-between;
+                    padding:8px 0;
+                    margin-top:3px
                   "
                 >
-                  <span>${text.value}</span>
-                  <span>${number.value}</span>
+                  <span style="color:#555">${text.value}</span>
+                  <span style="padding-right:10px">+ ${number.value}</span>
                 </div>
         `;
       inside_income.appendChild(render_income);
       text.value = "";
       number.value = "";
+      number.blur();
+      span_number.setAttribute("hidden", "");
+      number.style.borderColor = "#eee";
+    }
+    if (text.value != "" && number.value != "" && input_value.value == "-") {
+      let render_expenses = document.createElement("div");
+      render_expenses.innerHTML = `
+        <div
+                  class="render-expenses"
+                  style="
+                    border-top: 1px solid #eee;
+                    width: 100%;
+                    display: flex;
+                    justify-content: space-between;
+                    padding:8px 0;
+                    margin-top:3px
+                  "
+                >
+                  <span style="color:#555">${text.value}</span>
+                  <span style="padding-right:10px">- ${number.value}</span>
+                </div>
+        `;
+      inside_expenses.appendChild(render_expenses);
+      text.value = "";
+      number.value = "";
     }
   }
+};
+input_value.onchange = function () {
+  if (input_value.value === "-") {
+    check_icon.style.color = "#ff3859";
+  }
+  if (input_value.value === "+") {
+    text.oninput = function () {
+      text.style.borderColor = "#fba90a";
+    };
+    number.oninput = function () {
+      number.style.borderColor = "#fba90a";
+    };
+  }
+  if (input_value.value === "-") {
+    text.oninput = function () {
+      text.style.borderColor = "#ff3859";
+    };
+    number.oninput = function () {
+      number.style.borderColor = "#ff3859";
+    };
+  }
+};
+
+text.onblur = function () {
+  text.style.borderColor = "#eee";
+};
+number.onblur = function () {
+  text.style.borderColor = "#eee";
 };
